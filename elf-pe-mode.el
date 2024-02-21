399ab8f00b33d586ffd623eb01989f32e7f002d7
l_chayka <leanid.chaika@gmail.com>
2024-02-21 18:11:32 +0300

    start minimal sample to play with

--- elf-pe-mode.el
+++ elf-pe-mode.el
@@ -1,4 +1,22 @@
+;; windows: dumpbin /exports <file.dll>                  ;; dump all functions in dll or exe
+;; windows: dumpbin /symbols <file>
+;; windows: dumpbin /ARCHIVEMEMBERS <file.lib>           ;; dump list of all object files in static library
+;; windows: dumpbin /imports <foo.exe> | msvcfilt | less ;; complex example to demangle all symbols
+;; windows: lib.exe /list <file.lib>                     ;; dump list of all object files in static library
+;; unix:    ar t <libfile.a>
+;;
+;;
 
 (defgroup elf-pe-mode nil
   "Major mode for viewing iported/exported symbols in binary files with PE or ELF format."
   :group 'emacs)
+
+;;;###autoload
+(defun elf-pe-mode ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (insert (shell-command-to-string (format "readelf --syms -W %s" (buffer-file-name))))
+    (set-buffer-modified-p nil)
+    (read-only-mode 1)
+    ))
